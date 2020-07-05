47571c1525915ac1395cab574ef55edf9fbb856a
unknown <silvestre.santiago732@gmail.com>
2020-07-05 19:44:21 -0300

    show

--- src/main/java/com/desafio/ambev/api/dto/EstabelecimentoUpdateDTO.java
+++ src/main/java/com/desafio/ambev/api/dto/EstabelecimentoUpdateDTO.java
@@ -4,6 +4,9 @@ package com.desafio.ambev.api.dto;
 
 import javax.validation.constraints.NotNull;
 
+import com.desafio.ambev.domain.entity.Endereco;
+import com.desafio.ambev.domain.entity.Proprietario;
+
 public class EstabelecimentoUpdateDTO {
 
 	private Long id;
@@ -13,6 +16,25 @@ public class EstabelecimentoUpdateDTO {
 	
 	private String qtdBanheiros;
 	
+	@NotNull
+	private Proprietario proprietario;
+	
+	@NotNull
+	private Endereco endereco;
+	
+	public Endereco getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+	
+	public Proprietario getProprietario() {
+		return proprietario;
+	}
+	public void setProprietario(Proprietario proprietario) {
+		this.proprietario = proprietario;
+	}
 	
 	public Long getId() {
 		return id;
