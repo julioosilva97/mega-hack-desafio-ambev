8121d3aa22d014d5968373ba75d7a8b7b21ff366
Julio Cesar <julioosilva97@gmail.com>
2020-07-05 02:11:12 -0300

    show

--- src/main/java/com/desafio/ambev/core/WebConfig.java
+++ src/main/java/com/desafio/ambev/core/WebConfig.java
@@ -3,6 +3,7 @@ package com.desafio.ambev.core;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -13,5 +14,19 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+		
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+	    registry
+	            .addResourceHandler("swagger-ui.html")
+	            .addResourceLocations("classpath:/META-INF/resources/");
+
+	    registry
+        .addResourceHandler("/webjars/**")
+        .addResourceLocations("classpath:/META-INF/resources/webjars/");
+	   
 	}
 }
